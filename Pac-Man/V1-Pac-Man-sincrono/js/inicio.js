c76f48b8edbf7fa49ef1e06273cc049fa130b35b
cmaroto307 <109908350+cmaroto307@users.noreply.github.com>
2022-10-17 14:02:24 +0200

    Delete Pac-Man directory

--- Pac-Man/V1-Pac-Man-sincrono/js/inicio.js
+++
@@ -1,7 +0,0 @@
-var FILAS = 10;
-var COLUMNAS = 10;
-var VALORMURO = 1;
-var VALORLIBRE = 0;
-var VALOREXPLORADO = -1;
-
-var nuevaPartida = new pacman.Pacman("idTablero", "idMensaje", "idBotonIniPartida");
