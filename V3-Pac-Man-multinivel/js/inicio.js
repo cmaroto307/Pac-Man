5edec199f614d6531720c16d7f568344ca1fb0bd
cmaroto307 <109908350+cmaroto307@users.noreply.github.com>
2022-10-18 20:12:21 +0200

    Add files via upload

---
+++ V3-Pac-Man-multinivel/js/inicio.js
@@ -0,0 +1,9 @@
+var FILAS = 10;
+var COLUMNAS = FILAS;
+var NIVELES = 3;
+var NIVELACTUAL = 0;
+var VALORMURO = 1;
+var VALORLIBRE = 0;
+var VALOREXPLORADO = -1;
+
+var nuevaPartida = new pacman.Pacman("idTablero", "idMensaje", "idBotonIniPartida");
