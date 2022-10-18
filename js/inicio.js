263e7e1699bb859bd64239c6a3da64ce8f099f20
cmaroto307 <109908350+cmaroto307@users.noreply.github.com>
2022-10-18 21:25:26 +0200

    Versión penúltima

---
+++ js/inicio.js
@@ -0,0 +1,8 @@
+var FILAS = 10;
+var COLUMNAS = FILAS;
+var NIVELES = 3;
+var VALORMURO = 1;
+var VALORLIBRE = 0;
+var VALOREXPLORADO = -1;
+
+var nuevaPartida = new pacman.Pacman("idTablero", "idMensaje", "idBotonIniPartida");
